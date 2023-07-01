2a3028024e92ac1084d549a009af3662406795b8
Hila <hiladolev1@gmail.com>
2023-07-01 16:23:05 +0300

    createBrowserRouter + prevent editPage within valid vacation id

--- frontend/src/Components/Routes/MainRoute/MainRoute.tsx
+++ frontend/src/Components/Routes/MainRoute/MainRoute.tsx
@@ -1,4 +1,11 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+  redirect,
+} from "react-router-dom";
 import Add from "../../Pages/AddVacation/AddVacation";
 import Page404 from "../../Pages/Page404/Page404";
 import Edit from "../../Pages/Edit/Edit";
@@ -8,6 +15,54 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/TravelApp";
 import TinyBarChart from "../../Pages/Reports/TinyBarChart";
 import SignUp from "../../Pages/Register/SignUp";
+import MainLayout from "../../Layout/MainLayout/MainLayout";
+import axios from "axios";
+{
+  /* <Route
+      element={<Team />}
+      path="teams/:teamId"
+      loader={async ({ params }) => {
+        return fetch(`/fake/api/teams/${params.teamId}.json`);
+      }}
+      action={async ({ request }) => {
+        return updateFakeTeam(await request.formData());
+      }}
+      errorElement={<ErrorBoundary />}
+    /> */
+}
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<MainLayout />}>
+      <Route path="/" element={<VacationsPage />} />
+      <Route path="/add" element={<Add />} />
+      <Route path="/page404" element={<Page404 />} />
+      <Route
+        path="/edit/:id"
+        element={<Edit />}
+        loader={async ({ params }) => {
+          const result = await axios
+            .get(
+              `${process.env.REACT_APP_API_URL}/api/v1/vacations/getVacationById/${params.id}`
+            )
+            .then((response) => response.data);
+          if (result.length === 0) {
+            return redirect("/page404");
+          }
+          return null;
+        }}
+      />
+      <Route path="/reports" element={<TinyBarChart />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+
+      {/* <Route
+          path="*"
+          element={!status ? <Navigate to="/login" replace /> : <Page404 />}
+        /> */}
+    </Route>
+  )
+);
 
 function MainRoute(): JSX.Element {
   const currentUser = useSelector(
