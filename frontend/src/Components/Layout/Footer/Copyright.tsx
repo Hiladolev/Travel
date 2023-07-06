530b83b458811759080f452a328955bbe32c50ce
Hila <hiladolev1@gmail.com>
2023-07-06 17:56:21 +0300

    splitting copyright and footer into 2 components

---
+++ frontend/src/Components/Layout/Footer/Copyright.tsx
@@ -0,0 +1,21 @@
+import { Typography } from "@mui/material";
+import { pink } from "@mui/material/colors";
+import Link from "@mui/material/Link";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+export default function Copyright() {
+  return (
+    <Typography
+      variant="body2"
+      color="primary.contrastText"
+      fontFamily={["Roboto", "Helvetica", "Arial", "sans-serif"]}
+    >
+      Made with <FavoriteIcon sx={{ color: pink[500] }} /> By &nbsp;
+      <Link color="primary.contrastText" href="https://github.com/Hiladolev">
+        Hila Dolev
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
--- frontend/src/Components/Layout/Footer/Footer.tsx
+++ frontend/src/Components/Layout/Footer/Footer.tsx
@@ -1,28 +1,8 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import { pink } from "@mui/material/colors";
-
-function Copyright() {
-  return (
-    <Typography
-      variant="body2"
-      color="primary.contrastText"
-      fontFamily={["Roboto", "Helvetica", "Arial", "sans-serif"]}
-    >
-      Made with <FavoriteIcon sx={{ color: pink[500] }} /> By &nbsp;
-      <Link color="primary.contrastText" href="https://github.com/Hiladolev">
-        Hila Dolev
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import Copyright from "./Copyright";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
@@ -38,7 +18,7 @@ export default function Footer() {
           py: 3,
           px: 2,
           mt: "auto",
-          bgcolor: "info.main",
+          bgcolor: "primary.main",
         }}
       >
         <Container maxWidth="sm">
