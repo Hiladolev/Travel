411906014c1e81789236a48a201e817e298dca06
Hila <hiladolev1@gmail.com>
2023-07-08 20:40:26 +0300

    clean state -preview and selectedFile

--- frontend/src/Components/Pages/AddVacation/UploadImage.tsx
+++ frontend/src/Components/Pages/AddVacation/UploadImage.tsx
@@ -7,32 +7,23 @@ import { useEffect, useState } from "react";
 interface UploadImageProps {
   register: UseFormRegister<Vacation>;
 }
-
+const defaultImage =
+  "https://www.lifewire.com/thmb/TRGYpWa4KzxUt1Fkgr3FqjOd6VQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cloud-upload-a30f385a928e44e199a62210d578375a.jpg";
 function UploadImage({ register }: UploadImageProps): JSX.Element {
-  const [selectedFile, setSelectedFile] = useState<File>();
-  const [preview, setPreview] = useState<string>();
+  const [preview, setPreview] = useState<string>(defaultImage);
 
   useEffect(() => {
-    if (!selectedFile) {
-      setPreview(undefined);
-      return;
-    }
-
-    const objectUrl = URL.createObjectURL(selectedFile);
-    setPreview(objectUrl);
-
     // free memory when ever this component is unmounted
-    return () => URL.revokeObjectURL(objectUrl);
-  }, [selectedFile]);
+    // return () => URL.revokeObjectURL(objectUrl);
+  }, []);
 
   const onSelectFile = (e: any): void => {
     if (!e.target.files || e.target.files.length === 0) {
-      setSelectedFile(undefined);
       return;
     }
-
-    // I've kept this example simple by using the first image instead of multiple
-    setSelectedFile(e.target.files[0]);
+    const file = e.target.files[0];
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
   };
   return (
     <>
@@ -51,35 +42,19 @@ function UploadImage({ register }: UploadImageProps): JSX.Element {
           onChange={onSelectFile}
         />
       </Button>
-      {selectedFile ? (
-        <Grid item xs={4}>
-          <img
-            src={preview}
-            width={240}
-            style={{
-              maxHeight: 160,
-              border: "1px solid black",
-              borderRadius: "10px",
-            }}
-            alt="vacation"
-          />
-        </Grid>
-      ) : (
-        <Grid item xs={6}>
-          <img
-            src={
-              "https://www.lifewire.com/thmb/TRGYpWa4KzxUt1Fkgr3FqjOd6VQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/cloud-upload-a30f385a928e44e199a62210d578375a.jpg"
-            }
-            width={240}
-            style={{
-              maxHeight: 160,
-              border: "1px solid black",
-              borderRadius: "10px",
-            }}
-            alt="upload sign"
-          />
-        </Grid>
-      )}
+
+      <Grid item xs={4}>
+        <img
+          src={preview}
+          width={240}
+          style={{
+            maxHeight: 160,
+            border: "1px solid black",
+            borderRadius: "10px",
+          }}
+          alt="vacation"
+        />
+      </Grid>
     </>
   );
 }
