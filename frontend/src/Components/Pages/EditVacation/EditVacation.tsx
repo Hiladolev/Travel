3b7b7bfd93f3971d78c7d63d49956ec3208a507d
Hila <hiladolev1@gmail.com>
2023-07-12 12:59:45 +0300

    moving image control to UploadImage component +creating default values...

--- frontend/src/Components/Pages/EditVacation/EditVacation.tsx
+++ frontend/src/Components/Pages/EditVacation/EditVacation.tsx
@@ -1,11 +1,9 @@
 import { useNavigate, useParams, useLoaderData } from "react-router-dom";
-import { useForm } from "react-hook-form";
 import { updateVacationAction } from "../../Redux/VacationReducer";
 import axios from "axios";
-import { Button, TextField } from "@mui/material";
-import { ChangeEvent, useEffect, useState } from "react";
-import moment from "moment";
+import { Box, Container, CssBaseline, Typography } from "@mui/material";
 import { useDispatch } from "react-redux";
+import VacationForm from "../VacationForm/VacationForm";
 
 type updateVac = {
   destination: string;
@@ -17,63 +15,16 @@ type updateVac = {
   id?: number;
 };
 function EditVacation(): JSX.Element {
-  const loaderData: any = useLoaderData();
+  const loaderData = useLoaderData() as updateVac;
+
   const dispatch = useDispatch();
-  const [selectedFile, setSelectedFile] = useState<File>();
-  const [preview, setPreview] = useState<undefined | string>();
-  const today: string = moment().format("YYYY-MM-DD");
-  const [minStartDate, setMinStartDate] = useState<string>();
   const navigate = useNavigate();
   const params = useParams();
 
-  const onChangeStartDate = (event: ChangeEvent<HTMLInputElement>) => {
-    const selectedDate = moment(event.target.value).format("YYYY-MM-DD");
-    setMinStartDate(selectedDate);
-  };
-  const formatDate = (date: Date): string => {
+  const formatDate = (date: string | Date): string => {
     const formattedDate = new Date(date).toISOString().split("T")[0];
     return formattedDate;
   };
-  const getVacationById = async () => {
-    const vacationInfo = loaderData;
-    reset({
-      ...vacationInfo,
-      startDate: formatDate(vacationInfo.startDate),
-      endDate: formatDate(vacationInfo.endDate),
-    });
-    setPreview(`${process.env.REACT_APP_API_URL}/images/${vacationInfo.image}`);
-  };
-  useEffect(() => {
-    getVacationById();
-    // eslint-disable-next-line
-  }, []);
-  useEffect(() => {
-    if (!selectedFile) {
-      return;
-    }
-
-    const objectUrl = URL.createObjectURL(selectedFile);
-    setPreview(objectUrl);
-
-    // free memory when ever this component is unmounted
-    return () => URL.revokeObjectURL(objectUrl);
-  }, [selectedFile]);
-
-  const onSelectFile = (e: any) => {
-    if (!e.target.files || e.target.files.length === 0) {
-      setSelectedFile(undefined);
-      return;
-    }
-    // I've kept this example simple by using the first image instead of multiple
-    setSelectedFile(e.target.files[0]);
-  };
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<updateVac>();
 
   const editNewVacation = (updatedVacation: updateVac) => {
     const formControl: FormData = new FormData();
@@ -83,7 +34,12 @@ function EditVacation(): JSX.Element {
     formControl.append("endDate", updatedVacation.endDate.toString());
     formControl.append("price", updatedVacation.price.toString());
     formControl.append("id", params.id.toString());
-    formControl.append("image", selectedFile);
+    formControl.append(
+      "image",
+      typeof updatedVacation.image === "string"
+        ? undefined
+        : (updatedVacation.image as unknown as FileList)?.item(0)
+    );
     axios
       .put(
         `${process.env.REACT_APP_API_URL}/api/v1/vacations/edit`,
@@ -92,7 +48,7 @@ function EditVacation(): JSX.Element {
       .then((response) => {
         const finalVacation = response.data;
         if (!finalVacation.image) {
-          finalVacation.image = updatedVacation.image;
+          finalVacation.image = loaderData.image;
         }
         const changedVac = {
           ...finalVacation,
@@ -105,82 +61,29 @@ function EditVacation(): JSX.Element {
   };
 
   return (
-    <div className="Edit">
-      <h2>Edit Vacation</h2>
-      <form onSubmit={handleSubmit(editNewVacation)}>
-        <TextField
-          sx={{
-            width: 300,
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Edit Vacation
+        </Typography>
+        <VacationForm
+          vacationActionFunction={editNewVacation}
+          defaultValues={{
+            destination: loaderData.destination,
+            description: loaderData.description,
+            startDate: formatDate(loaderData.startDate),
+            endDate: formatDate(loaderData.endDate),
+            price: loaderData.price,
+            image: `${process.env.REACT_APP_API_URL}/images/${loaderData.image}`,
           }}
-          InputLabelProps={{ shrink: true }}
-          label="Destination"
-          variant="outlined"
-          required
-          {...register("destination")}
         />
-        <br /> <br />
-        <TextField
-          sx={{
-            width: 300,
-          }}
-          InputLabelProps={{ shrink: true }}
-          multiline
-          rows={2}
-          label="Description"
-          required
-          {...register("description")}
-        />
-        <br /> <br />
-        <input
-          required
-          type="date"
-          {...register("startDate")}
-          min={today}
-          onChange={onChangeStartDate}
-        />
-        <br /> <br />
-        <input
-          required
-          type="date"
-          {...register("endDate")}
-          min={minStartDate}
-        />
-        <br /> <br />
-        <TextField
-          sx={{
-            width: 300,
-          }}
-          InputLabelProps={{ shrink: true }}
-          label="Price"
-          type="number"
-          placeholder="$"
-          required
-          {...register("price", { valueAsNumber: true, min: 0, max: 10000 })}
-        />
-        {errors.price && (
-          <div style={{ color: "red" }}>Limit Price is $10,000</div>
-        )}
-        <br /> <br />
-        <Button component="label" {...register("image")}>
-          Change Image
-          <input
-            hidden
-            accept="image/*"
-            type="file"
-            name="image"
-            onChange={onSelectFile}
-          />
-        </Button>
-        <span>
-          <img src={preview} width={200} alt="vacation" />
-        </span>
-        <br />
-        <br />
-        <Button variant="contained" type="submit">
-          Update Vacation
-        </Button>
-      </form>
-    </div>
+      </Box>
+    </Container>
   );
 }
 
