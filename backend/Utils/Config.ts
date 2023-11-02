34f4c5082768208b1c83e7bee3024cae6391c0b3
Hila <hiladolev1@gmail.com>
2023-11-02 15:17:52 +0200

    changing sql host for trying to connect to ubuntu server

--- backend/Utils/Config.ts
+++ backend/Utils/Config.ts
@@ -1,6 +1,6 @@
 class Config {
   public WebPort = process.env.PORT;
-  public mySQLhost = process.env.MYSQL_HOST;
+  public mySQLhost = "127.0.0.1";
   public mySQLuser = process.env.MYSQL_USER;
   public mySQLpass = process.env.MYSQL_PASSWORD;
   public mySQLdatabase = process.env.MYSQL_DATABASE;
