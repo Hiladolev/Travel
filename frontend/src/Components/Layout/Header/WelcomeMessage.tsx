3b7e6b4024c6811a068ff3eac7c0381165dfbeb3
Hila Dolev <hiladolev1@gmail.com>
2023-12-28 15:16:34 +0200

    changing user message from div into typography with paragraph attribute and css declarations

--- frontend/src/Components/Layout/Header/WelcomeMessage.tsx
+++ frontend/src/Components/Layout/Header/WelcomeMessage.tsx
@@ -1,15 +1,21 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/TravelApp";
+import Typography from "@mui/material/Typography";
 
 function WelcomeMessage(): JSX.Element {
   const currentUser = useSelector(
     (state: RootState) => state.users.currentUser
   );
   return (
-    <div style={{ marginLeft: "auto" }}>
+    <Typography
+      color="inherit"
+      paragraph
+      sx={{ fontFamily: "sans-serif" }}
+      marginTop={"1.3%"}
+    >
       Welcome,
       {` ${currentUser.firstName} ${currentUser.lastName}  `}
-    </div>
+    </Typography>
   );
 }
 export default WelcomeMessage;
