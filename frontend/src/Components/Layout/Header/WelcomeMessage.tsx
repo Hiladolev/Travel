41efa5e84e57def56b739135307f6919053c40d5
Hila Dolev <hiladolev1@gmail.com>
2023-12-30 16:54:03 +0200

    ternary operator for displaying full name of the user only when the theme is in size xl

--- frontend/src/Components/Layout/Header/WelcomeMessage.tsx
+++ frontend/src/Components/Layout/Header/WelcomeMessage.tsx
@@ -1,8 +1,13 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/TravelApp";
 import Typography from "@mui/material/Typography";
+import { useTheme, Theme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function WelcomeMessage(): JSX.Element {
+  const theme = useTheme() as Theme;
+  const isXlg = useMediaQuery(theme.breakpoints.up("xl"));
+  const displayMessage = isXlg ? "inline" : "none";
   const currentUser = useSelector(
     (state: RootState) => state.users.currentUser
   );
@@ -13,6 +18,7 @@ function WelcomeMessage(): JSX.Element {
       sx={{
         fontFamily: "sans-serif",
         marginBlockStart: "5%",
+        display: displayMessage,
       }}
     >
       {` ${currentUser.firstName} ${currentUser.lastName}  `}
